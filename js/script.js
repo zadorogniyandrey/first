b9aaa45164fffe217c7750bd8633c56840614ae9
zadorogniyandrey <103739570+zadorogniyandrey@users.noreply.github.com>
2022-07-14 15:55:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+$(document).ready(function(){
+    $('.baner').slick({
+        autoplay:  true,
+        dots: true,
+        fade: true,
+        appendArrows:'.slider-arrows',
+        appendDots: '.slider-dots',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+    });
+   
+    $(".advantage__item").on("click", function () {
+        $(".advantage__item").removeClass('selected')
+        $(this).addClass('selected');
+    });
+    const iconMenu = document.querySelector('.menu__btn');
+    const menuBody = document.querySelector('.header__top');
+    if (iconMenu) {
+        iconMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle('_lock');
+            iconMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        });
+    }
+  });
